dc4995724365793de84b6d7bb0e40652a96900bb
Reid Welliver <reidwelliver@gmail.com>
2022-07-17 22:25:22 -0600

    add bundler and react bootstrapping for frontend

---
+++ frontend/webpack/dev.js
@@ -0,0 +1,47 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const common = require("./common");
+
+module.exports = {
+  ...common,
+  devtool: "inline-source-map",
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "development",
+      template: path.resolve("src", "html", "index.dev.html"),
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+    new ReactRefreshWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              plugins: [require.resolve("react-refresh/babel")],
+            },
+          },
+        ],
+      },
+      ...common.module.rules,
+    ],
+  },
+  output: {
+    path: "/",
+    publicPath: "/",
+    filename: "[name].pkg.js",
+    chunkFilename: "[name].pkg.js",
+  },
+  devServer: {
+    hot: true,
+    port: 3000,
+    historyApiFallback: true,
+  },
+};
---
+++ frontend/webpack/prod.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
+const common = require("./common");
+
+module.exports = {
+  ...common,
+  mode: "production",
+  optimization: {
+    minimize: false,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "development",
+      template: path.resolve("src", "html", "index.prod.html"),
+    }),
+    new ForkTsCheckerWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      ...common.module.rules,
+    ],
+  },
+  output: {
+    path: path.resolve("dist"),
+    filename: "[name].pkg.js",
+    publicPath: "/",
+    chunkFilename: "[name].pkg.js",
+  },
+  externals: [
+    {
+      react: "React",
+      "react-dom": "ReactDOM",
+    },
+  ],
+};
