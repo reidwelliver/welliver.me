1273301a8bcb32c85d6a26556082034c187957a5
Reid Welliver <reidwelliver@gmail.com>
2022-07-18 17:38:49 -0600

    fix getBaseURL to work with production deployment

--- frontend/src/api/getBaseURL.ts
+++ frontend/src/api/getBaseURL.ts
@@ -1,8 +1,9 @@
-const devUrlExtension = ":7071/api";
+const devUrlPort = ":7071";
+const apiDirectory = "/api";
 
 export default function getBaseUrl(): string {
   const { protocol, hostname } = window.location;
   const isDev = protocol === "http:";
 
-  return `${protocol}//${hostname}${isDev ? devUrlExtension : ""}`;
+  return `${protocol}//${hostname}${isDev ? devUrlPort : ""}${apiDirectory}`;
 }
